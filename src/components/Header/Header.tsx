4a385c38a5014c9b570437d9d6bc8f41ded4074c
isefrog38 <isefroge38@gmail.com>
2022-02-27 15:54:38 +0300

    fix button and flex blocks

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,11 +5,11 @@ export const Header = () => {
     return (
         <div className={s.headerParent}>
             <div className={s.headerChild}>
-                    <a href="#">Home</a>
-                    <a href="#">Skills</a>
-                    <a href="#">Freelance</a>
-                    <a href="#">Projects</a>
-                    <a href="#">Contact</a>
+                    <a className={s.menuItem} href="#">Home</a>
+                    <a className={s.menuItem} href="#">Skills</a>
+                    <a className={s.menuItem} href="#">Freelance</a>
+                    <a className={s.menuItem} href="#">Projects</a>
+                    <a className={s.menuItem} href="#">Contact</a>
             </div>
         </div>
     );
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -7,11 +7,17 @@ export const Main = () => {
         <div className={s.main}>
                 <div className={s.mainContainer}>
                     <div className={s.HomeInfo}>
-                        <span>Hello There</span>
-                        <h1>I am Pavel Kuharchik</h1>
-                        <p>Frontend Developer</p>
+                        <span className={s.spanText}>Hello There, I'm</span>
+                        <h1 className={s.Name}>Pavel Kuharchik</h1>
+                        <p className={s.whoIam}>Frontend <b style={{color: "#f9564f"}}>Developer</b></p>
+                        <div className={s.CNBlock}>
+                            <a className={s.socialNetwork} href="#">f</a>
+                            <a className={s.socialNetwork} href="#">t</a>
+                            <a className={s.socialNetwork} href="#">in</a>
+                            <a className={s.socialNetwork} href="#">@</a>
+                        </div>
                     </div>
-                    <div className={s.HomeInfoPhoto}></div>
+                    <div className={s.HomeInfoPhoto}/>
                 </div>
         </div>
     );
